f4137927a8a8de0a9e95221334a4a6ed91ef69e7
wilkinvsquez <wilkinvsquez@gmail.com>
2025-04-26 22:27:55 -0600

    feat(app/[username]/page.tsx): showing the information by user with all the repositories on its profile

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,17 +1,32 @@
 import { getUser, getUserRepos } from "@/services/githubAPI";
+import { getCommitCount } from "@/services/githubAPI";
+import { DynamicUserProps } from "@/Interfaces/DynamicUserProps";
+import { Dashboard } from "@/components/Dashboard";
 
-interface Props {
-	params: { username: string };
-}
+import { UserSkeleton } from "@/components/Skeletons/UserSkeleton";
+import { RepoListSkeleton } from "@/components/Skeletons/RepoListSekeleton";
+import { DashboardSkeleton } from "@/components/Skeletons/DashboardSkeleton";
+import { RepoList } from "@/components/RepoList";
 
-export default async function UserPage({ params }: Props) {
+export default async function UserPage({ params }: DynamicUserProps) {
 	const { username } = await params;
-
-	let userData, reposData;
-
+	let userData: any, reposData: any;
+	userData = await getUser(username);
 	try {
-		userData = await getUser(username);
 		reposData = await getUserRepos(username);
+		reposData = await Promise.all(
+			reposData.map(async (repo: any) => {
+				try {
+					const commitCount = await getCommitCount(
+						userData.login,
+						repo.name,
+					);
+					return { ...repo, commits: commitCount };
+				} catch {
+					return { ...repo, commits: 0 };
+				}
+			}),
+		);
 	} catch (error) {
 		<div className='p-4 text-center'>
 			<h2 className='text-xl font-bold text-red-600'>
@@ -20,37 +35,51 @@ export default async function UserPage({ params }: Props) {
 		</div>;
 	}
 	return (
-		<main className='p-6 max-w-4xl mx-auto space-y-6'>
-			<section className='bg-white shadow rounded p-4'>
-				<h1 className='text-2xl font-bold'>
-					{userData.name || userData.login}
-				</h1>
-				<p>{userData.bio}</p>
-				<p>Repos públicos: {userData.public_repos}</p>
-				<p>
-					Seguidores: {userData.followers} · Siguiendo:{" "}
-					{userData.following}
-				</p>
+		<main className='p-6  max-w-4xl mx-auto space-y-8'>
+			{/* Perfil de usuario */}
+			<section className='p-8 bg-gray-700 rounded-2xl shadow-lg flex flex-col sm:flex-row items-center sm:items-start gap-6 hover:shadow-xl hover:bg-gray-500 transition-all'>
+				<img
+					src={userData.avatar_url}
+					alt='Avatar'
+					className='w-32 h-32 rounded-full border-4 border-slate-300 shadow-md'
+				/>
+				<div className='text-center sm:text-left space-y-2'>
+					<h1 className='text-3xl font-bold text-white'>
+						{userData.name}
+					</h1>
+					<p className='text-lg text-gray-400'>@{userData.login}</p>
+
+					<div className='flex flex-wrap justify-center sm:justify-start gap-6 text-sm text-gray-400 mt-4'>
+						<span className='flex items-center gap-1'>
+							👥 <strong>{userData.followers}</strong> followers
+						</span>
+						<span className='flex items-center gap-1'>
+							📦 <strong>{userData.public_repos}</strong> repos
+						</span>
+						<span className='flex items-center gap-1'>
+							🏡 {userData.location || "Unknown"}
+						</span>
+					</div>
+
+					{userData.bio && (
+						<p className='mt-6 text-gray-400 max-w-lg mx-auto sm:mx-0'>
+							{userData.bio}
+						</p>
+					)}
+				</div>
 			</section>
 
+			{/* Repositorios */}
+			<section className='space-y-4'>
+				<h2 className='text-xl font-semibold text-gray-400'>
+					Repositorios
+				</h2>
+				<RepoList repos={reposData} />
+			</section>
+
+			{/* Dashboard de estadísticas */}
 			<section>
-				<h2 className='text-xl font-semibold mb-2'>Repositorios</h2>
-				<ul className='grid gap-4'>
-					{reposData.map((repo: any) => (
-						<li
-							key={repo.id}
-							className='bg-white rounded shadow p-4'>
-							<h3 className='font-bold text-lg'>{repo.name}</h3>
-							<p className='text-sm text-gray-600'>
-								{repo.description}
-							</p>
-							<p className='text-sm mt-2'>
-								⭐ {repo.stargazers_count} · 🍴{" "}
-								{repo.forks_count} · 🧪 {repo.language}
-							</p>
-						</li>
-					))}
-				</ul>
+				<Dashboard repos={reposData} />
 			</section>
 		</main>
 	);
