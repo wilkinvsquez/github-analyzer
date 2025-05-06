5e4603388ac9e5cfa5e0ed6d03ddd3410b9251ed
wilkinvsquez <wilkinvsquez@gmail.com>
2025-05-05 21:37:11 -0600

    removing export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  basePath: '/github-analyzer',
-  //assetPrefix: '/github-analyzer/',
-  trailingSlash: true,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
